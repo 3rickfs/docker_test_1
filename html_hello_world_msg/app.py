cdb00c1ef84d34c3b323f6246f985836d282e6a3
Erick Fiestas Sorogastúa <efiestass@upao.edu.pe>
2018-11-26 23:34:42 -0500

    Update app.py

--- html_hello_world_msg/app.py
+++ html_hello_world_msg/app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello wonderful {name}!</h3>" \
+    html = "<h3>Hello overwhelming {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.$
